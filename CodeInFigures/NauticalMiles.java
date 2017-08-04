f028073718b03ef3400f3996d1ad7cdf73ce17c2
jessebridge <jesse.bridge@my.jcu.edu.au>
2017-08-04 21:21:26 +1000

    Completed task where nautical miles is set to a constant variable as per the task sheet

--- CodeInFigures/NauticalMiles.java
+++ CodeInFigures/NauticalMiles.java
@@ -1,15 +1,8 @@
-import javax.swing.*;
-// Adapted the required program so that you could put your own input in to calculate the distance, instead of having just 1 set distance that it would always calculate
-
 public class NauticalMiles {
     public static void main(String[] args) {
-        double KILOMETER = 1.852, MILES = 1.150779,UserInputToDouble;
-        String NauticalMilesString;
-        NauticalMilesString = JOptionPane.showInputDialog(null, "Please insert the number of Nautical miles you wish to convert to km or miles");
-        UserInputToDouble = Double.parseDouble(NauticalMilesString);
-        double NauticalMileToMile = UserInputToDouble * MILES, NauticalMileToKm = UserInputToDouble * KILOMETER;
-        JOptionPane.showMessageDialog(null, UserInputToDouble + " nautical miles are equal to " + NauticalMileToKm +" kilometers and " + NauticalMileToMile + " Miles"  );
-
+        double KILOMETER = 1.852, MILES = 1.150779, NAUTICALMILESTOCALCULATE = 12, CalculationOfKm, CalculationOfMiles;
+        CalculationOfKm = KILOMETER * NAUTICALMILESTOCALCULATE;
+        CalculationOfMiles = MILES * NAUTICALMILESTOCALCULATE;
+        System.out.println(NAUTICALMILESTOCALCULATE + " Nautical miles is equal to " + CalculationOfKm + " Kilometers and " + CalculationOfMiles + " Miles");
     }
 }
-
