b7d2ea5ca27e19c041bd778b8f6cd97aad1c94e8
jessebridge <jesse.bridge@my.jcu.edu.au>
2017-08-04 20:36:01 +1000

    Completed the debugging exercises now i need to complete the second task

--- DebuggingExercises/DebugTwo4.java
+++ DebuggingExercises/DebugTwo4.java
@@ -1,15 +1,14 @@
 import javax.swing.JOptionPane;
-public class DebugTwo4
-{
-   public static void main(String[] args)
-   {
-      String costString
-      double cost;
-      final double TAX = 0.06;
-      costString = JOptionPane.showInputDialog("Enter price of item you are buying", "Purchases",
-         JOptionPane.INFORMATION_MESSAGE);
-      cost = Double.parseInt(costString);
-      JOptionPane.showMessageDialog(null,"With " + tax * 100 +
-         "% tax,  purchase  is $" + (cost - cost * tax));
-   }
+
+public class DebugTwo4 {
+    public static void main(String[] args) {
+        String coststring;
+        double cost;
+        final double tax = 0.06;
+        coststring = JOptionPane.showInputDialog(null,"Enter price of item you are buying", "Purchases",
+                JOptionPane.INFORMATION_MESSAGE);
+        cost = Double.parseDouble(coststring);
+        JOptionPane.showMessageDialog(null, "With " + tax * 100 +
+                "% tax,  purchase  is $" + (cost - cost * tax));
+    }
 }
